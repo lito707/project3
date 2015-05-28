28ecb14693b470bf6981d7ee5521905bcb252d05
lito707 <lito707@hotmail.com>
2015-05-28 21:43:49 +1000

    Predictions controller and model finished, need to be polished and view

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,31 +1,54 @@
 class Prediction < ActiveRecord::Base
 	def predict_by_coordinates(lat, long, period)
-		locations = get_closest_locations(lat, long)
+		locations_w_dist = get_closest_locations(lat, long)
 
-		if locations.first[1] == 0 # Distance == 0, it matches exactly with a location of the database
-			data = location_data(locations.first[0], period)
-
-			rain_model = find_model(data[:rain])
-			wind_dir_model = find_model(data[:wind_dir])
-			wind_speed_model = find_model(data[:wind_speed])
-			temperature_model = find_model(data[:temperature])
-
-			a = predict(rain_model, period)
-			b = predict(wind_dir_model, period)
-			c = predict(wind_speed_model, period)
-			d = predict(temperature_model, period)
-
-			return [a,b,c,d]
+		if locations_w_dist.first[1] == 0 # Distance == 0, it matches exactly with a location of the database
+			get_all_predictions(locations_w_dist.first[0], period)
 		else
-			return predict_3N(locations)
+			results = [[],[],[],[]]
+			probabilities = []
+
+			first_prob, first_set = get_all_predictions(locations_w_dist[0][0], period)
+			second_prob, second_set = get_all_predictions(locations_w_dist[1][0], period)
+			third_prob, third_set = get_all_predictions(locations_w_dist[2][0], period)
+
+			factors = get_factors(locations_w_dist)
+
+			(0...first_set.length).to_a.each do |i|
+				(0...first_set[i].length).to_a.each do |j|
+					results[i][j] = (first_set[i][j]*factors[0] + second_set[i][j]*factors[1] + third_set[i][j]*factors[2])/(factors[0]+factors[1]+factors[2])
+				end
+				probabilities[i] = (first_prob[i]*factors[0] + second_prob[i]*factors[1] + third_prob[i]*factors[2])/(factors[0]+factors[1]+factors[2])
+			end
+			return probabilities, results
 		end
 	end
 
-	def predict_3N(locations)
-		sum_distances = locations.inject(0) { |sum, i| sum + locations[i][1] }
+	def get_factors(locations_w_dist)
+		sum_distances = (0...locations_w_dist.length).inject(0) { |sum, i| sum + locations_w_dist[i][1] }
+		dist1 = locations_w_dist[0][1]
+		dist2 = locations_w_dist[1][1]
+		dist3 = locations_w_dist[2][1]
 
-		
+		return [dist1/sum_distances, dist2/sum_distances, dist3/sum_distances]
+	end
 
+	def get_all_predictions(location, period) # get an array of 4 arrays with the predicted values of wind speed, wind dir, temp and rainfall
+		data = location_data(location, period)
+
+		rain_model = find_model(data[:rain])
+		wind_dir_model = find_model(data[:wind_dir])
+		wind_speed_model = find_model(data[:wind_speed])
+		temperature_model = find_model(data[:temperature])
+
+		probabilities = [rain_model[:probability],wind_dir_model[:probability],wind_speed_model[:probability],temperature_model[:probability]]
+
+		a = predict(rain_model, period)
+		b = predict(wind_dir_model, period)
+		c = predict(wind_speed_model, period)
+		d = predict(temperature_model, period)
+
+		return probabilities, [a,b,c,d]
 	end
 
 	def location_data(location, period)
